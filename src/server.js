e02651ffff40f05e023f815de654c973c420cae2
coopsie <aislingkcooper@hotmail.com>
2023-02-02 14:42:09 +0000

    update pakage.json file

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ require("./db/connection");
 const express = require("express");
 const userRouter = require("./user/userRoutes");
 
+const cors = require ("cors");
+
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
 app.use(userRouter);
+
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
